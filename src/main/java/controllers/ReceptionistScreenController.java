fb0de10570b7f9b2b9c5f814c3573899fa323ddb
firon <S.ISLOMOV@student.inha.uz>
2016-12-18 12:37:03 +0500

    receptionist bug fixed not manager

--- src/main/java/controllers/ReceptionistScreenController.java
+++ src/main/java/controllers/ReceptionistScreenController.java
@@ -2,10 +2,16 @@ package main.java.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 /**
  * Created by sardor on 12/15/16.
@@ -72,4 +78,15 @@ public class ReceptionistScreenController {
     void onDeleteClicked(ActionEvent event) {
 
     }
+
+    @FXML
+    void onExitClicked(ActionEvent event) throws IOException {
+        Stage stage  = new Stage();
+        Parent rootHotel = FXMLLoader.load(getClass().getResource("/main/layout/login_screen.fxml"));
+        Scene scene = new Scene(rootHotel);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+
 }
